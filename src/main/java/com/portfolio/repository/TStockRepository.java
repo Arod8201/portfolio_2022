2360dabcc2bb5b8afeebd1412b96c9b91c61667d
arod8201 <arod8201@gmail.com>
2022-04-28 23:10:04 +0800

    portfolio

---
+++ src/main/java/com/portfolio/repository/TStockRepository.java
@@ -0,0 +1,38 @@
+package com.portfolio.repository;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.portfolio.entity.TStock;
+
+@Repository
+public interface TStockRepository extends JpaRepository<TStock, Integer> {
+
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE TStock SET name=?2, symbol=?3, classify_id=?4 WHERE id=?1", nativeQuery = true)
+	public void update(@Param("id") Integer id, 
+						@Param("name") String name, 
+						@Param("symbol") String symbol,
+					    @Param("classify_id") Integer classify_id);
+
+	@Transactional
+	@Modifying
+	@Query(value = "UPDATE TStock WHERE id=?1", nativeQuery = true)
+	public void updatePrice(@Param("id") Integer id, 
+							 @Param("changePrice") BigDecimal changePrice, 
+	    					 @Param("changeInPercent") BigDecimal changeInPercent,
+	    					 @Param("preClosed") BigDecimal preClosed, 
+	    					 @Param("price") BigDecimal price, 
+	    					 @Param("transactionDate") Date transactionDate, 
+	    					 @Param("volumn") Long volumn);
+
+}
---
+++ src/main/java/com/portfolio/repository/WatchRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.portfolio.entity.Investor;
+import com.portfolio.entity.Watch;
+
+@Repository
+public interface WatchRepository extends JpaRepository<Watch, Integer> {
+
+}
